279985a28df0808c3ba0261e5996bc87e16f6f1c
Dominik Przybysz <alien11689@gmail.com>
2025-04-05 20:40:13 +0200

    Add generating spring controller interface

---
+++ task-provider/src/test/java/dpr/playground/taskprovider/TaskProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package dpr.playground.taskprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
