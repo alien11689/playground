249fb01617cfef63989c7b481981db0e430e049d
Dominik Przybysz <alien11689@gmail.com>
2025-04-05 20:40:13 +0200

    Add generating spring controller interface

---
+++ task-provider/src/main/java/dpr/playground/taskprovider/TaskProviderApplication.java
@@ -0,0 +1,13 @@
+package dpr.playground.taskprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskProviderApplication.class, args);
+	}
+
+}
