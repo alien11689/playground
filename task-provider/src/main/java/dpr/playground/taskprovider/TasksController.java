bfdf737452c6d78347c27c95b5df74037ffbc53b
Dominik Przybysz <alien11689@gmail.com>
2025-04-16 19:04:36 +0200

    Add suffix to model classes

--- task-provider/src/main/java/dpr/playground/taskprovider/TasksController.java
+++ task-provider/src/main/java/dpr/playground/taskprovider/TasksController.java
@@ -1,12 +1,12 @@
 package dpr.playground.taskprovider;
 
 import dpr.playground.taskprovider.tasks.api.TasksApi;
-import dpr.playground.taskprovider.tasks.model.AddTaskCommentRequest;
-import dpr.playground.taskprovider.tasks.model.AddTaskRequest;
-import dpr.playground.taskprovider.tasks.model.Comment;
-import dpr.playground.taskprovider.tasks.model.GetTaskCommentsResponse;
-import dpr.playground.taskprovider.tasks.model.GetTasksResponse;
-import dpr.playground.taskprovider.tasks.model.Task;
+import dpr.playground.taskprovider.tasks.model.AddTaskCommentRequestDTO;
+import dpr.playground.taskprovider.tasks.model.AddTaskRequestDTO;
+import dpr.playground.taskprovider.tasks.model.CommentDTO;
+import dpr.playground.taskprovider.tasks.model.GetTaskCommentsResponseDTO;
+import dpr.playground.taskprovider.tasks.model.GetTasksResponseDTO;
+import dpr.playground.taskprovider.tasks.model.TaskDTO;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +16,12 @@ import java.util.UUID;
 @RestController
 public class TasksController implements TasksApi {
     @Override
-    public ResponseEntity<Task> addTask(AddTaskRequest addTaskRequest) {
+    public ResponseEntity<TaskDTO> addTask(AddTaskRequestDTO addTaskRequest) {
         throw new RuntimeException("Not implemented");
     }
 
     @Override
-    public ResponseEntity<Comment> addTaskComment(UUID taskId, AddTaskCommentRequest addTaskCommentRequest) {
+    public ResponseEntity<CommentDTO> addTaskComment(UUID taskId, AddTaskCommentRequestDTO addTaskCommentRequest) {
         throw new RuntimeException("Not implemented");
     }
 
@@ -31,28 +31,28 @@ public class TasksController implements TasksApi {
     }
 
     @Override
-    public ResponseEntity<Task> getTask(UUID taskId) {
+    public ResponseEntity<TaskDTO> getTask(UUID taskId) {
         throw new RuntimeException("Not implemented");
     }
 
     @Override
-    public ResponseEntity<GetTaskCommentsResponse> getTaskComments(UUID taskId, Integer page, Integer size, Pageable pageable) {
+    public ResponseEntity<GetTaskCommentsResponseDTO> getTaskComments(UUID taskId, Integer page, Integer size, Pageable pageable) {
         throw new RuntimeException("Not implemented");
     }
 
     @Override
-    public ResponseEntity<GetTasksResponse> getTasks(Integer page, Integer size, Pageable pageable) {
+    public ResponseEntity<GetTasksResponseDTO> getTasks(Integer page, Integer size, Pageable pageable) {
         // TODO finish
-        return ResponseEntity.ok(new GetTasksResponse());
+        return ResponseEntity.ok(new GetTasksResponseDTO());
     }
 
     @Override
-    public ResponseEntity<Void> updateTask(UUID taskId, Task task) {
+    public ResponseEntity<Void> updateTask(UUID taskId, TaskDTO task) {
         throw new RuntimeException("Not implemented");
     }
 
     @Override
-    public ResponseEntity<Void> updateTaskComment(UUID taskId, UUID commentId, Comment comment) {
+    public ResponseEntity<Void> updateTaskComment(UUID taskId, UUID commentId, CommentDTO comment) {
         throw new RuntimeException("Not implemented");
     }
 }
--- task-provider/src/main/java/dpr/playground/taskprovider/auth/LoginController.java
+++ task-provider/src/main/java/dpr/playground/taskprovider/auth/LoginController.java
@@ -2,7 +2,7 @@ package dpr.playground.taskprovider.auth;
 
 import dpr.playground.taskprovider.Database;
 import dpr.playground.taskprovider.tasks.api.LoginApi;
-import dpr.playground.taskprovider.tasks.model.LoginResponse;
+import dpr.playground.taskprovider.tasks.model.LoginResponseDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -13,8 +13,8 @@ import java.util.UUID;
 @RestController
 class LoginController implements LoginApi {
     @Override
-    public ResponseEntity<LoginResponse> login() {
-        LoginResponse loginResponse = new LoginResponse();
+    public ResponseEntity<LoginResponseDTO> login() {
+        LoginResponseDTO loginResponse = new LoginResponseDTO();
         String token = UUID.randomUUID().toString();
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Database.saveToken(token, user.getUsername());
