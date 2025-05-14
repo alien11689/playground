3c5bbfe1f25280d968fd09a35a1473df53b1c00f
Dominik Przybysz <alien11689@gmail.com>
2025-05-14 21:54:39 +0200

    Add clock bean

--- task-provider/src/main/java/dpr/playground/taskprovider/TaskProviderApplication.java
+++ task-provider/src/main/java/dpr/playground/taskprovider/TaskProviderApplication.java
@@ -1,10 +1,18 @@
 package dpr.playground.taskprovider;
 
+import java.time.Clock;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskProviderApplication {
+
+    @Bean
+    Clock clock() {
+        return Clock.systemUTC();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TaskProviderApplication.class, args);
